f35761d1f7ff42c21d8f8bc8eeaea2ab92f0a64e
Szymon Stypa <szymon@backtick.se>
2023-11-07 11:04:34 +0100

    windfury by bibbs

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,13 @@ const SPEED = 5
 #const JUMP_VELOCITY = 4
 const TURN_SPEED = 2
 const ROTATION_SPEED = 20
+const WF_PROC_RATE = 0.2 # VERY DANGEROUS INCREASE WITH CAUTION!
 
 @onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var animation_tree : AnimationTree = $Pivot.get_node("Farmer/AnimationTree2")
 @onready var healthbar : ProgressBar = $Pivot.get_node("Farmer/SubViewport/Healthbar")
 
-var crit_chance = 0.05
+var crit_chance = 0.30 # 5% critchance is to weak
 var health = 100
 var direction : Vector3 = Vector3.ZERO
 var on_floor : bool = false
@@ -18,6 +19,9 @@ var moving = false
 var attacking = false
 var kicking = false
 var is_dead = false
+
+var rng = RandomNumberGenerator.new()
+
 
 func take_damage(damage: int, crit: bool):
 	health = clamp(health - damage, 0, 100)
@@ -37,14 +41,30 @@ func roll():
 	
 	timeout(duration, func (): rolling = false)
 
+
 func attack():
+	# Set attack to true
 	attacking = true
+
 	
 	const duration = 0.3
 	
-	# spawn hitbox
+	# Attack may result in  wf proc
 	
-	timeout(duration, func (): attacking = false)
+	#if rng.randf() <= WF_PROC_RATE:
+	#	animation_tree["parameters/RunSwing/SwingScale/scale"] = SPEED * 3.5
+	#	animation_tree["parameters/Swing/SwingScale/scale"] = SPEED * 3.5
+	
+	# spawn hitbox
+	if rng.randf() <= WF_PROC_RATE:
+		animation_tree["parameters/Slash/QuickBlend/blend_amount"] = 1
+	
+	# Reset flags after duration
+	timeout(duration,
+		func ():
+			attacking = false
+			animation_tree["parameters/Slash/QuickBlend/blend_amount"] = 0
+	)
 	
 func kick():
 	kicking = true
@@ -60,6 +80,9 @@ func _ready():
 	animation_tree.active = true
 	animation_tree["parameters/Roll/RollScale/scale"] = SPEED / 2.7
 	animation_tree["parameters/RunSwing/RunScale/scale"] = SPEED / 3.5
+	animation_tree["parameters/Slash/StateMachine/QuickSlash1/TimeScale/scale"] = 7.5
+	animation_tree["parameters/Slash/StateMachine/QuickSlash2/TimeScale/scale"] = 7.5
+	animation_tree["parameters/Slash/StateMachine/QuickSlash3/TimeScale/scale"] = 7.5
 
 func _process(delta):
 	healthbar.value = health
@@ -74,8 +97,8 @@ func animate():
 	
 	animation_tree["parameters/conditions/idle"] = not moving
 	animation_tree["parameters/conditions/moving"] = moving
-		
-	animation_tree["parameters/conditions/swing"] = attacking
+	
+	animation_tree["parameters/conditions/swing"] = attacking 
 	animation_tree["parameters/conditions/kick"] = kicking
 	
 	if moving and attacking and not animation_tree["parameters/RunSwing/OneShot/active"]:
@@ -122,6 +145,6 @@ func _physics_process(delta):
 
 func _on_area_3d_body_exited(body):
 	if body.has_method("take_damage") and body.is_in_group("enemies"):
-		var is_crit = RandomNumberGenerator.new().randf_range(0, 1) <= crit_chance
+		var is_crit = rng.randf() <= crit_chance
 		var multiplier = 2 if is_crit else 1
-		body.take_damage(RandomNumberGenerator.new().randi_range(10, 15) * multiplier, is_crit, transform.basis.z)
+		body.take_damage(rng.randi_range(10, 15) * multiplier, is_crit, transform.basis.z)
